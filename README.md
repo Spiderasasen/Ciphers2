f4cf5ac1270aa121535c2736ba36f824cd722df4
spiderasasen <spiderasasen@gmail.com>
2025-12-29 10:42:01 -0500

    basic idea of reading a text file

--- README.md
+++ README.md
@@ -7,5 +7,17 @@ Nothing. its still the same idea, but for another ARG
 # how would this work in the other arg
 Simple, we will use wasm. but first i need to make the code first
 
-# streamed as well
-because im lonly
+for the code, i know wasm love to print out all print statements, so for print statements that are not ment to be shown. I will just comment out, but still include in this file
+
+# distinguishing each cipher
+
+1. for base 64
+   1. each base 64 file contains a =, so its checked first.
+   2. if the file has a =, then returns a decipher base 64 cipher
+2. if binary
+    1. bit tricky
+        1. first checks for the majority of the cipher.
+       2. if the cipher is majority 1 and 0, then return binary decipher message
+       3. else returns a deciphered ceaser cipher.
+3. if ceaser
+   1. just returns a deciphered ceaser cipher.
--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello world")
+from typing import List
+
+def checkingMajority(cipher: str) -> bool:
+    ceaser: List[str] = [] #empty ceaser cipher list
+    binary: List[str] = [] #empty binary list
+
+    for char in cipher:
+        if char == "0" or char == "1":
+            binary.append(char)
+        else:
+            ceaser.append(char)
+
+    if len(ceaser) > len(binary): #if the ceaser has the majority, returns true
+        return True
+    else: #if binary has the majoity, returns false
+        return False
+
+if __name__ == '__main__':
+    cipher: str = input("Please enter the cipher here\n")
+
+    if "=" in cipher:
+        print("base64")
+    elif ("0" in cipher) and ("1" in cipher):
+        print("binary?")
+
+        #checks the majoirty of chars in the cipher to see which cipher to use
+        if checkingMajority(cipher):
+            print("No, its ceaser")
+        else:
+            print("yes, its binary")
+    else:
+        print("ceaser")
